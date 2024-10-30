2d742e7ea713c4c945b54f10efe4ce10cc6a1696
ashanatta <muhammadashan1714@gmail.com>
2024-10-30 14:40:56 +0500

    Implemented Books and Authors CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\RentalController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('rentals', RentalController::class);
